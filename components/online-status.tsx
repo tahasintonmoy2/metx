922662a767144372e55965c3fe516983741350d2
Tahasin <110173793+tahasintonmoy2@users.noreply.github.com>
2023-12-28 18:44:12 +0600

    Update online-status.tsx

--- components/online-status.tsx
+++ components/online-status.tsx
@@ -1,30 +1,21 @@
 "use client";
 
-import { Member } from "@prisma/client";
-import React, {useState, useEffect} from 'react'
 import { useSocket } from "./providers/socket-provider";
 
-interface OnlineStatusProps {
-    member?: Member
-}
+export const OnlineStatus = () => {
+  const { isOnline } = useSocket();
 
-
-export const OnlineStatus = ({
- member
-}: OnlineStatusProps) => {
-    const { socket, isOnline } = useSocket();
-
-    if (!isOnline) {
-       return (
-        <p className="dark:text-zinc-400 text-zinc-500 ml-1 font-semibold text-[12px]">
-         - is Offline 
-        </p>
-       )
-    }
+  if (!isOnline) {
+    return (
+      <p className="dark:text-zinc-400 md:block hidden text-zinc-500 ml-1 font-semibold text-[12px]">
+        - is Offline
+      </p>
+    )
+  }
 
   return (
     <p className="text-green-500 ml-1 font-semibold text-[12px]">
-       - is Online 
+      - is Online
     </p>
   )
 }
